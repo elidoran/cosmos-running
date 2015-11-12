8d8f946fea05594eb2555fe9d2b32cfb51c764be
Eli Doran <eli@elidoran.com>
2015-11-12 16:58:45 -0500

    update deps, use vars for scopes

--- package.js
+++ package.js
@@ -1,36 +1,37 @@
 Package.describe({
   name: 'cosmos:running',
-  version: '0.1.0',
+  version: '0.2.0',
   summary: 'Ordered startup functions ',
   git: 'http://github.com/elidoran/cosmos-running',
   documentation: 'README.md'
 });
 
+var cs = [ 'client', 'server' ]
+
 Package.onUse(function(api) {
-  api.versionsFrom('1.1');
+  api.versionsFrom('1.2');
 
   api.use([
     'tracker',       // onChange uses Tracker
     'reactive-var',  // _value is a ReactiveVar
-    'coffeescript@1.0.6',
-    'cosmos:chain@0.1.0'
-  ], ['client', 'server']);
+    'coffeescript@1.0.11',
+    'cosmos:chain@0.4.0'
+  ], cs);
 
   api.addFiles([
     'export.js',
     'running.coffee'
-  ], ['client', 'server']);
+  ], cs);
 
-  api.export('Running', ['client', 'server']);
+  api.export('Running', cs);
 });
 
 Package.onTest(function(api) {
-  api.use(['tinytest', 'coffeescript@1.0.6']);
+  api.use(['tinytest', 'coffeescript@1.0.11']);
 
   api.use('cosmos:running');
 
   api.addFiles([
     'test/running-tests.coffee'
-  ],
-  ['client', 'server']);
+  ], cs);
 });
